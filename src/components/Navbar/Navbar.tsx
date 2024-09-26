380e06064d170f57445b0ad079b091fe5734a4a1
Felipe Alves <felipealexandre1408@gmail.com>
2024-09-26 16:09:31 -0300

    Fix: Trying to fix deploy error.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
