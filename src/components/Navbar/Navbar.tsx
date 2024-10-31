1bdf3141ce801ff6078cdc06375b6bd36df506c0
Felipe Alves <felipealexandre1408@gmail.com>
2024-10-31 14:20:41 -0300

    Fix: Link component is now involving menu options for the mobile size. Now the application isn't refreshing anymore while going to another route in mobile."

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -114,18 +114,18 @@ export default function Navbar() {
       <DisclosurePanel className="sm:hidden">
         <div className="space-y-1 px-2 pb-3 pt-2">
           {navigation.map((item) => (
-            <DisclosureButton
-              key={item.name}
-              as="a"
-              href={item.href}
-              aria-current={item.current ? 'page' : undefined}
-              className={classNames(
-                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                'block rounded-md px-3 py-2 text-base font-medium',
-              )}
-            >
+            <Link key={item.name} to={item.href}>
+              <DisclosureButton
+                key={item.name}
+                aria-current={item.current ? 'page' : undefined}
+                className={classNames(
+                  item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                  'block rounded-md px-3 py-2 text-base font-medium',
+                )}
+              >
               {item.name}
             </DisclosureButton>
+            </Link>
           ))}
         </div>
       </DisclosurePanel>
